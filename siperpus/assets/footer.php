2a2851d79d0b87722443ed5bbf6116114893a20c
fadhilrazzan <54299052+fadhilrazzan@users.noreply.github.com>
2020-04-07 00:33:13 +0700

    Add files via upload

---
+++ siperpus/assets/footer.php
@@ -0,0 +1,5 @@
+<script src="http://localhost/UKL/assets/js/jquery.min.js"></script>
+   <script src="http://localhost/UKL/assets/js/bootstrap.min.js"></script>
+   <script src="https://kit.fontawesome.com/7de1522d08.js" crossorigin="anonymous"></script>
+</body>
+</html>
